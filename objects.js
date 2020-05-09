c6c590b695b68c941ca58954b47347a5b8acff30
Harold Lee <harry.yg.lee@gmail.com>
2020-05-09 15:59:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,6 @@ const playlist={artistName:'song'}
 function updatePlaylist(playlist, artistName,songTitle){
   return playlist[artistName]='songTitle'
 }
+function removeFromPlaylist(playlist,artistName){
+  return delete.playlist['artistName']
+}
